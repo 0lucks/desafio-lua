8c182f1dd3ca4fffa8dd59fbe006b50d6deae8ce
0lucks <0lucks@users.noreply.github.com>
2023-12-07 14:57:43 -0300

    Update desafio.lua

--- desafio.lua
+++ desafio.lua
@@ -29,17 +29,17 @@ agilidade
 ]]
 --Criatura
 local monsterName = "Gato"
-local descricao = "Um animal preguiçoso e agil"
+local monsterClass = "Animal"
+local descricao = "Um Animal preguiçoso e agil"
 
 --Atributos
 local attackPontos = 4
 local defensePontos = 3
-local lifePontos = 4
+local lifePontos = 7
 local speedPontos = 9
 local inteligencePontos = 7
 
 --function
-
 local function getProgressBar(attribute)
     local fullChar = "⬜"
     local emptyChar = "⬛"
@@ -56,16 +56,16 @@ local function getProgressBar(attribute)
 end
 
 --Cartão
-
-print("======================================")
-print("|".. monsterName)
-print("|".. descricao)
+print("==========================================")
+print("| Nome: ".. monsterName)
+print("| Clase: ".. monsterClass)
+print("| Descrição: ".. descricao)
 print("|")
 print("| Atributos")
-print("|  Attack:       ".. getProgressBar(attackPontos))
+print("|  Ataque:       ".. getProgressBar(attackPontos))
 print("|  Defesa:       ".. getProgressBar(defensePontos))
 print("|  Vida:         ".. getProgressBar(lifePontos))
 print("|  Velicidade:   ".. getProgressBar(speedPontos))
 print("|  Inteligencia: ".. getProgressBar(inteligencePontos))
 print("|")
-print("==================================")
+print("==========================================")
